fb5dbbb1776c88e3d393c049c6d1b3a6e116cc6a
Vicx95 <wiktor.zyla31@gmail.com>
2020-09-20 16:51:26 +0200

    Implement AppendNewRecipe function

--- module4/homework/appendNewRecipe/AppendNewRecipe.cpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -1,1 +1,21 @@
 #include "AppendNewRecipe.hpp"
+#include <algorithm>
+#include <fstream>
+#include <map>
+
+static const std::map<char, std::string> quantities{{'g', "gram"},
+                                                    {'s', "szklanka(i)"},
+                                                    {'m', "mililitrow"}};
+
+bool AppendNewRecipe(std::vector<std::string> steps,
+                     const std::list<std::string>& ingredients,
+                     const std::deque<std::pair<std::size_t, char>>& amount) {
+    std::string fileName = "recipes.txt";
+    std::fstream recipesStream(fileName, recipesStream.out | recipesStream.app);
+    if (!recipesStream.is_open()) {
+        return false;
+    }
+    recipesStream << FormatRecipit(steps, ingredients, amount).str();
+    recipesStream.close();
+    return true;
+}
